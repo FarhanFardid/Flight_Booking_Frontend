f138a0d415fa7741bb09165b78db6be1d915c634
FarhanFardid <farhanfardid009@gmail.com>
2024-11-28 16:30:58 +0600

    Banner component updates

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,4 +1,6 @@
+import { FaArrowRight } from "react-icons/fa";
 import img1 from "../../assets/images/planeImg/booking-flight.jpg";
+
 const Banner = () => {
   return (
     <div className="container-fluid hero h-[478px] p-5 md:p-16 top-[20px] md:top-[221px] ">
@@ -20,10 +22,9 @@ const Banner = () => {
           <div className="flex md: flex-row w-[464px] h-[64px] items-center justify-center">
             <button
               type="button"
-              className="text-white bg-black 
-          w-[120px] h-[45px] md:w-[130px] md:h-[52px] hover:bg-slate-800  font-medium rounded-3xl text-xs md:text-sm px-5 py-2.5 text-center me-2 mb-2 "
+             className="flex items-center justify-center space-x-2 text-white bg-black w-[170px] h-[45px] md:w-[190px] md:h-[52px] hover:bg-slate-800 font-medium rounded-2xl text-xs md:text-sm px-5 py-2.5 text-center me-2 mb-2"
             >
-              Book A Flight Now
+              Book A Flight Now <FaArrowRight className="text-white ms-2"/>
             </button>
           </div>
           <div className="flex flex-row justify-center md:justify-normal "></div>
