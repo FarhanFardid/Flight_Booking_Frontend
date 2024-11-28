f7c9949722dfb9b4a25b389109e1d755015c36ce
FarhanFardid <farhanfardid009@gmail.com>
2024-11-28 12:35:39 +0600

    Navigation shared component added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
+import Navigation from "../Shared/Navigation";
 
 const Main = () => {
-    return (
-        <div>
-            Main page
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <div>
+      <Navigation></Navigation>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/Shared/Navigation.jsx
@@ -0,0 +1,10 @@
+
+const Navigation = () => {
+    return (
+        <div>
+            This is Navigation bar
+        </div>
+    );
+};
+
+export default Navigation;
