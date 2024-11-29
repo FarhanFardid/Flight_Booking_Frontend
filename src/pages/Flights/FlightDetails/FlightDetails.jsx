b167ff9bf9a67ef867594526fc21a425931a2366
FarhanFardid <farhanfardid009@gmail.com>
2024-11-29 14:52:38 +0600

    Flight Results and FlightDetails page Title compo updates

--- src/pages/Flights/FlightDetails/FlightDetails.jsx
+++ src/pages/Flights/FlightDetails/FlightDetails.jsx
@@ -1,10 +1,16 @@
+import Title from "../../../components/Title";
 
 const FlightDetails = () => {
-    return (
-        <div>
-            Flight Details Page
-        </div>
-    );
+  return (
+    <>
+      <div className="py-8 md:py-12">
+        <Title
+          main="Flight Information Overview"
+          sub="All the information you need for the selected flight."
+        ></Title>
+      </div>
+    </>
+  );
 };
 
 export default FlightDetails;
--- src/pages/Flights/FlightResults/FlightResults.jsx
+++ src/pages/Flights/FlightResults/FlightResults.jsx
@@ -1,10 +1,16 @@
+import Title from "../../../components/Title";
 
 const FlightResults = () => {
-    return (
-        <div>
-            Flight Result Page
-        </div>
-    );
+  return (
+    <>
+      <div className="py-8 md:py-12">
+        <Title
+          main="Your Flight Awaits"
+          sub="Discover available flights to your desired destination."
+        ></Title>
+      </div>
+    </>
+  );
 };
 
 export default FlightResults;
