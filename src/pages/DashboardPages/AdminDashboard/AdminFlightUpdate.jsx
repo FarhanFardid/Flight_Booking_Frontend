1f62ccaa27cbc23d048b761bc6ab137b8bf77626
FarhanFardid <farhanfardid009@gmail.com>
2024-11-29 22:48:05 +0600

    Admin FLIGHT UPDATE page IMPLEMENTATION UNDER GOING

--- src/pages/DashboardPages/AdminDashboard/AdminFlightUpdate.jsx
+++ src/pages/DashboardPages/AdminDashboard/AdminFlightUpdate.jsx
@@ -1,11 +1,20 @@
-
+import backgroundImage from "../../../assets/images/planeImg/plane3.jpg";
+import Title from "../../../components/Title";
 
 const AdminFlightUpdate = () => {
-    return (
-        <div>
-            Admin Flight Update page
-        </div>
-    );
+  return (
+    <div
+      className="bg-no-repeat bg-center bg-cover p-2 md:p-8 max-h-full md:h-full"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="mt-6 md:mt-12 bg-white bg-opacity-10 p-2 md:p-8 rounded-md">
+        <Title
+          main="Update Flight Details"
+          sub="Efficiently Manage and Modify Flight Information"
+        ></Title>
+      </div>
+    </div>
+  );
 };
 
 export default AdminFlightUpdate;
