77df664dc2b576cf18fcf80265dd6973bc1104e4
FarhanFardid <farhanfardid009@gmail.com>
2024-11-29 22:32:14 +0600

    Admin dashboard page updates

--- src/pages/DashboardPages/AdminDashboard/AdminBookingUpdate.jsx
+++ src/pages/DashboardPages/AdminDashboard/AdminBookingUpdate.jsx
@@ -1,11 +1,20 @@
-
+import backgroundImage from "../../../assets/images/planeImg/plane3.jpg";
+import Title from "../../../components/Title";
 
 const AdminBookingUpdate = () => {
-    return (
-        <div>
-            Admin Booking Update page
-        </div>
-    );
+  return (
+    <div
+      className="bg-no-repeat bg-center bg-cover p-2 md:p-8 max-h-full md:h-full"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="mt-6 md:mt-12 bg-white bg-opacity-10 p-2 md:p-8 rounded-md">
+        <Title
+          main="Update Flight Bookings"
+          sub="Manage and Modify Existing Flight Reservations with Ease"
+        ></Title>
+      </div>
+    </div>
+  );
 };
 
 export default AdminBookingUpdate;
--- src/pages/DashboardPages/AdminDashboard/AdminFlightManagement.jsx
+++ src/pages/DashboardPages/AdminDashboard/AdminFlightManagement.jsx
@@ -63,7 +63,7 @@ const AdminFlightManagement = () => {
 
   return (
     <div
-      className="custom-bg bg-no-repeat bg-center bg-cover p-2 md:p-8 max-h-full md:h-full"
+      className="bg-no-repeat bg-center bg-cover p-2 md:p-8 max-h-full md:h-full"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="mt-6 md:mt-12 bg-white bg-opacity-10 p-2 md:p-8 rounded-md">
