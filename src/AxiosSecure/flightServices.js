9fad28fe959f24a4da9d6c2a55635e7e99d958c0
FarhanFardid <farhanfardid009@gmail.com>
2024-11-30 19:41:21 +0600

    api services instaces integrated with axiosInstance

---
+++ src/AxiosSecure/flightServices.js
@@ -0,0 +1,39 @@
+import axiosInstance from "./axiosInstance";
+
+// ---------------- Fetch all flights data --------------------------
+export const getAllFlights = async () => {
+  const response = await axiosInstance.get("/flights");
+  return response.data;
+};
+
+//-------------------- Fetch  specific flight data -----------------
+export const getFlightById = async (id) => {
+  const response = await axiosInstance.get(`/flights/${id}`);
+  return response.data;
+};
+
+// ------------------- Flights searching data on criteria -------------------
+export const searchFlights = async (query) => {
+  const response = await axiosInstance.get("/flights/search", {
+    params: query,
+  });
+  return response.data;
+};
+
+// --------------------------- Send new flights data ------------------------
+export const addFlight = async (flightData) => {
+  const response = await axiosInstance.post("/flights", flightData);
+  return response.data;
+};
+
+// ------------------------- Update flight data -------------------
+export const updateFlight = async (id, flightData) => {
+  const response = await axiosInstance.put(`/flights/${id}`, flightData);
+  return response.data;
+};
+
+// ----------------------- Delete a flight data ---------------------------
+export const deleteFlight = async (id) => {
+  const response = await axiosInstance.delete(`/flights/${id}`);
+  return response.data;
+};
