6daa497969999437e165e216f4ac5eaa0d676fa0
FarhanFardid <farhanfardid009@gmail.com>
2024-12-01 15:34:59 +0600

    User dashboard profile page implemented with original data from server

--- src/pages/DashboardPages/UserDashboard/UserProfile.jsx
+++ src/pages/DashboardPages/UserDashboard/UserProfile.jsx
@@ -1,12 +1,28 @@
+import { useContext, useEffect, useState } from "react";
 import backgroundImage from "../../../assets/images/planeImg/plane1.jpg";
 import Title from "../../../components/Title";
+import { AuthContext } from "../../../providers/AuthProvider/AuthContext";
+import { getUserInfo } from "../../Authentication/auth";
 
 const UserProfile = () => {
-  const userDetails = {
-    userId: 101,
-    userName: "User1",
-    userEmail: "user1@gmail.com",
-  };
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  const userId = user?.id;
+  console.log(userId);
+  const [userDetails, setUserDetails] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await getUserInfo(userId);
+        console.log("Response:", response);
+        setUserDetails(response);
+      } catch (err) {
+        console.error("Error details:", err.response?.data || err.message);
+        toast.error("Flights Data Load Failed");
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div
       className="bg-no-repeat bg-center bg-cover p-2 md:p-8 max-h-full md:h-full"
@@ -23,14 +39,14 @@ const UserProfile = () => {
           <div className="w-full">
             <div className="details bg-white bg-opacity-50 text-black p-3 md:p-6 rounded-md">
               <h2 className="text-xs md:text-sm font-medium pb-1 md:pb-2 ">
-                User Id: {userDetails.userId}
+                User Id: {userDetails._id}
               </h2>
               <h2 className="text-base md:text-xl font-bold py-2 md:py-3 ">
-                User Name: {userDetails.userName}
+                User Name: {userDetails.username}
               </h2>
               <p className="font-medium pb-2 text-xs md:text-sm">
                 <strong>Email:</strong>{" "}
-                <span id="display-email">{userDetails.userEmail}</span>
+                <span id="display-email">{userDetails.email}</span>
               </p>
             </div>
           </div>
